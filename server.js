61cf8be87293949429f03b3d1d37dcfa62f3d0fd
Elisheva <elishevaelbaz1@gmail.com>
2019-02-07 16:21:59 -0500

    added connection/disconnection and functional messaging

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 var http = require('http').Server(app);
-var io = req('socket.io')(http);
+var io = require('socket.io')(http);
  
 var PORT = 3000;
 
@@ -11,8 +11,19 @@ app.get('/', function (req, res) {
 })
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  // console.log('a user connected');
+  // socket.on('disconnect', function(){
+  // 	console.log('a user disconnected');
+
+
+  socket.on('chat message', function(msg){
+  	console.log('message: ' + msg);
+  	io.emit('chat message', msg);
+  	
+  });
+  
 });
+// });
  
 http.listen(PORT, () => {
 	console.log(`App listening on PORT: ${PORT}`)
